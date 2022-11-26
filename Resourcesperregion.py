3076753273600d1078f211fbb1b865aad014aa53
oriongalaxy <105608312+oriongalaxy@users.noreply.github.com>
2022-11-26 12:06:02 -0700

    Update Resourcesperregion.py

--- Resourcesperregion.py
+++ Resourcesperregion.py
@@ -1,7 +1,7 @@
 import boto3
+#utilizing boto3 api service for ec2. reference https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/index.html
 client = boto3.client('resourcegroupstaggingapi')
 client.get_resources()
-#print(client.get_resources()['ResourceTagMappingList'])
 for item in client.get_resources()['ResourceTagMappingList']:
     region = str(item).split(':')[4]
     print(region, '---', item['ResourceARN'])
