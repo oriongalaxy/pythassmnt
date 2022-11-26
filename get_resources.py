bf14578ff35aed38ca0f50d693bbada56f017742
oriongalaxy <105608312+oriongalaxy@users.noreply.github.com>
2022-11-26 03:17:08 -0700

    Delete get_resources.py

--- get_resources.py
+++
@@ -1,7 +0,0 @@
-import boto3
-client = boto3.client('resourcegroupstaggingapi')
-client.get_resources()
-responses = client.get_resources()
-
-for response in responses['ResourceTagMappingList']:
-    print(response['ResourceARN'])
